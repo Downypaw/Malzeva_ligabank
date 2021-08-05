e2337ca184f1a75ae1c54dbfaf940043136261b5
Таня <tanamalceva274@gmail.com>
2021-08-05 18:16:20 +0300

    Добавит разметку и стили для шапки

--- project/src/components/header/header.jsx
+++ project/src/components/header/header.jsx
@@ -3,45 +3,41 @@ import React from 'react';
 export default function Header() {
   return (
     <header className="header">
-      <div className="container">
-        <div className="header__wrapper">
-          <div className="header__left">
-            <a className="header__logo-link" href="#">
-              <img className="header__logo" src="img/logo.svg" alt="Логотип Лига Банк" />
-            </a>
-          </div>
-          <nav className="header__navigation navigation">
-            <ul className="navigation__list">
-              <li className="navigation__item">
-                <a className="navigation__link">
-                  Услуги
-                </a>
-              </li>
-              <li className="navigation__item">
-                <a className="navigation__link">
-                  Рассчитать кредит
-                </a>
-              </li>
-              <li className="navigation__item">
-                <a className="navigation__link">
-                  Конвертер валют
-                </a>
-              </li>
-              <li className="navigation__item">
-                <a className="navigation__link">
-                  Контакты
-                </a>
-              </li>
-              <li className="navigation__item">
-                <a className="navigation__link">
-                  Задать вопрос
-                </a>
-              </li>
-            </ul>
-          </nav>
-          <div className="header__login">
-            <a className="header__logo-link" href="#">Войти в Интернет-банк</a>
-          </div>
+      <div className="header__container container">
+        <div className="header__left">
+          <img className="header__logo" src="img/logo.svg" alt="Логотип Лига Банк" width="149" height="26"/>
+        </div>
+        <nav className="header__navigation">
+          <ul className="header__navigation-list">
+            <li className="header__navigation-item">
+              <a className="header__navigation-link" href="#">
+                Услуги
+              </a>
+            </li>
+            <li className="header__navigation-item">
+              <a className="header__navigation-link" href="#">
+                Рассчитать кредит
+              </a>
+            </li>
+            <li className="header__navigation-item">
+              <a className="header__navigation-link" href="#">
+                Конвертер валют
+              </a>
+            </li>
+            <li className="header__navigation-item">
+              <a className="header__navigation-link" href="#">
+                Контакты
+              </a>
+            </li>
+            <li className="header__navigation-item">
+              <a className="header__navigation-link" href="#">
+                Задать вопрос
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div className="header__login">
+          <a className="header__login-link" href="#">Войти в Интернет-банк</a>
         </div>
       </div>
     </header>
