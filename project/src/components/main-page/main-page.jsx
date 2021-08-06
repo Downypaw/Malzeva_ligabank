9ae0a4e917863ee34276c1092b48adca3d414b83
Таня <tanamalceva274@gmail.com>
2021-08-06 16:04:23 +0300

    Добавлен блок промо

--- project/src/components/main-page/main-page.jsx
+++ project/src/components/main-page/main-page.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../header/header';
 import Footer from '../footer/footer';
+import Promo from '../promo/promo';
 
 export default function MainPage() {
   return (
     <>
       <Header />
+      <Promo />
       <Footer />
     </>
   );
---
+++ project/src/components/promo/promo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <div className="promo">
+      <div className="container">
+        <h1 className="promo__headling">Лига Банк</h1>
+        <p className="promo__description">Кредиты на любой случай</p>
+        <a className="promo__link">Рассчитать кредит</a>
+      </div>
+    </div>
+  );
+}
